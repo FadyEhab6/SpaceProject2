a76425639b66e017215be36d78f0cd4094b13c1e
FadyEhab6 <jetalex95@gmail.com>
2024-09-18 16:57:44 +0300

    1st commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+// const input = prompt("Please enter your age")
+
+// document.getElementById("demo").innerHTML = input;
+
+console.log("IT SHOULD BE WORKING?!")
+//Testing for github upload
+//TEst 2
