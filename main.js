2f847c77b96813b3b14a4dff9f4cba159fd8c1ea
FadyEhab6 <jetalex95@gmail.com>
2024-09-19 21:09:13 +0300

    TEST 2

--- main.js
+++
@@ -1,8 +0,0 @@
-
-// const input = prompt("Please enter your age")
-
-// document.getElementById("demo").innerHTML = input;
-
-console.log("IT SHOULD BE WORKING?!")
-//Testing for github upload
-//TEst 2
---
+++ main.mjs
@@ -0,0 +1,23 @@
+import gameData from './gameData.json' assert{type: 'json'}
+const randomElement = (arr) => arr[Math.floor(Math.random() * arr.length)]; //generates a random element (index) from an array
+let isSuccessful = (successChance) => (Math.random() * 100 <= successChance); //returns bool if successfull
+let o2 = 100
+let days = 10
+
+//Object.keys(gameData.planets).forEach(key => console.log(key, gameData.planets[key]))
+
+function activateTest(testType){
+    const didSucceed = isSuccessful(gameData.tests[testType].successRate)? 'success' : 'failure' //determines if test was successful
+    const result = randomElement(gameData.tests[testType][didSucceed]); //gets a random response from the array of results
+    console.log(result);
+}
+
+activateTest('test4');
+
+setInterval(() => {
+    console.log(`O2% left: ${--o2}`)
+},(2000)); //countdown oxygen
+
+setInterval(() => {
+    console.log(`Days left: ${--days}`)
+},(3000)); //countdown days left
