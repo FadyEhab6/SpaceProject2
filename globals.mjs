f5f5e57ba85cf98912eb69072668ff0bb82743d5
FadyEhab6 <jetalex95@gmail.com>
2024-09-20 19:39:56 +0300

    globals.mjs file for variables + changes

---
+++ globals.mjs
@@ -0,0 +1,6 @@
+import planets from './planets.json' assert{type: 'json'}
+import { RandomElement } from './main.mjs';
+let chosenPlanets = Array.from({ length: 5 }, () => RandomElement(Object.values(planets))); //generates 5 random planets and adds them to chosenPlanets (AS OBJECTS)
+export let currentPlanet = "Planet1"; // Planet 1 is a placeholder for the text that comes from each planet button
+export let o2 = 100, days = 10, logs = [];
+export { chosenPlanets };
--- main.mjs
+++ main.mjs
@@ -1,31 +1,30 @@
 import gameData from './gameData.json' assert{type: 'json'}
+import planets from './planets.json' assert{type: 'json'}
+import { currentPlanet, chosenPlanets, o2, days, logs } from './globals.mjs';
 
-let currentPlanet = "Planet1"   //Planet 1 is a placeholder for the text that comes from each planet button
-let o2 = 100, days = 10, logs = []
 
-const RandomElement = (arr) => arr[Math.floor(Math.random() * arr.length)]; //generates a random element (index) from an array
+export function RandomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)]
+} //generates a random element (index) from an array
 let IsSuccessful = (successChance) => (Math.random() * 100 <= successChance); //returns bool if successfull
 function Log(message){
-    logs.push(message)
+    logs.push(`- [${currentPlanet}] ` + message)
 }
 function ReadLog(){
-    logs.forEach(log => console.log(log + '\n'))
+    console.log('PLANET DISCOVERY LOGS: \n')
+    logs.forEach(log => console.log(log))
 }
 async function activateTest(testType){
     const didSucceed = IsSuccessful(gameData.tests[testType].successRate)? 'success' : 'failure' //determines if test was successful
     const result = RandomElement(gameData.tests[testType][didSucceed]); //gets a random response from the array of results
     Log(result) 
     ReadLog() 
-} 
-
-function planetTester(currentPlanet){
-    let test = "test1"       //Test 1 is a placeholder for the text that comes from each test button
-    activateTest(test)
-} 
+}  
 function SwitchPlanet(planet){ //called from html button
     currentPlanet = planet
 }
-planetTester(currentPlanet)
+
+activateTest('test1') // The test is determined from html button, it will be called on currentPlanet
 //activateTest('test4'); 
 
 // setInterval(() => {
